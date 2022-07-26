1b9ec5a6f50fd9e539db1c9d589383c044f78d0f
洛千 <hezhilong.hzl@alibaba-inc.com>
2022-07-26 10:31:40 +0800

    init

---
+++ src/main/java/fun/ifish/script/spider/ScriptSpiderApplication.java
@@ -0,0 +1,13 @@
+package fun.ifish.script.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptSpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptSpiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/ifish/script/spider/ScriptSpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.ifish.script.spider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptSpiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
